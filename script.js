6eedd6b01fa3c9943e9a78d4bbbf6b1a8279f559
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-02-23 22:24:23 +0530

    remove unnecessary code and fix minor styling

--- script.js
+++ script.js
@@ -3,25 +3,19 @@ let button = document.getElementById('btn')
 let quote = document.getElementById('quote')
 let author = document.getElementById('author')
 
-
 const Quotes = async () => {
     try {
         let api = await fetch('https://type.fit/api/quotes')
         let res = await api.json()
-
         let random = Math.floor(Math.random() * 16)
 
-        res.forEach(e => {
-            quote.innerHTML = res[random].text
-            let Auth = res[random].author
-
-            let AuthRep = Auth.replace(', type.fit', '')
-            if (AuthRep.includes('type.fit')) {
-                AuthRep = 'Unknown'
-            }
-            author.innerHTML = `~&nbsp;By&nbsp;${AuthRep}`
-
-        });
+        // console.log(res[random])
+        quote.innerHTML = res[random].text
+        let Auth = res[random].author.replace(', type.fit', '')
+        if (Auth.includes('type.fit')) {
+            Auth = 'Unknown'
+        }
+        author.innerHTML = `~&nbsp;By&nbsp;${Auth}`;
     }
 
     catch (err) {
@@ -29,6 +23,7 @@ const Quotes = async () => {
     }
 
     let textToCopy = document.getElementById('quote').innerHTML
+
 
     // Twitter Web Intent
 
@@ -41,18 +36,14 @@ const Quotes = async () => {
 
     //  Copy to Clipboard Functionality
 
-
     let copyBtn = document.getElementById('copyBtn')
     let faRegular = document.getElementsByClassName('fa-regular')[0]
-    console.log(faRegular);
-    console.log(copyBtn);
-
+    // console.log(faRegular);
+    // console.log(copyBtn);
     copyBtn.addEventListener('click', (e) => {
         e.preventDefault()
         navigator.clipboard.writeText(textToCopy)
-
     })
-
 }
 
 button.addEventListener('click', Quotes)
@@ -64,12 +55,10 @@ let tooltip = document.getElementById('tooltip')
 
 copyBtn.addEventListener('click', (e) => {
     e.preventDefault()
-
     faRegular.classList.toggle('fa-solid')
     tooltip.style.visibility = 'visible'
     setTimeout(() => {
         faRegular.classList.remove('fa-solid')
         tooltip.style.visibility = 'hidden'
     }, 2000)
-
 })
