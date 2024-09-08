7b712350de033c9392b3d3b99ced2164232025fa
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-09-08 14:44:23 +0530

    changed expired api then added dark mode and enhaced the layout as well as cleaned the code

--- script.js
+++ script.js
@@ -1,64 +1,89 @@
-// https://type.fit/api/quotes
-let button = document.getElementById('btn')
-let quote = document.getElementById('quote')
-let author = document.getElementById('author')
+// api = https://dummyjson.com/quotes
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Declared All Variables and general function
+let generateButton = document.getElementById('btn');
+let quoteElement = document.getElementById('quote');
+let authorElement = document.getElementById('author');
+let copyBtn = document.getElementById('copyBtn');
+let faRegular = document.getElementsByClassName('fa-regular')[0];
+let tooltip = document.getElementById('tooltip');
+let body = document.body;
+let box = document.getElementsByClassName('box')[0]
+let darkModeBtn = document.getElementsByClassName('darkModeBtn')[0]
+const getStyle = (value) => window.getComputedStyle(value);
+
 
 const Quotes = async () => {
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Fetching API
     try {
-        let api = await fetch('https://type.fit/api/quotes')
+        let api = await fetch('https://dummyjson.com/quotes?limit=1400')
         let res = await api.json()
-        let random = Math.floor(Math.random() * 16)
+        let random = Math.floor(Math.random() * 1390)
+        let quote = res.quotes[random].quote;
+        let author = res.quotes[random].author;
+        // console.log('author:', author, typeof author)
 
-        // console.log(res[random])
-        quote.innerHTML = res[random].text
-        let Auth = res[random].author.replace(', type.fit', '')
-        if (Auth.includes('type.fit')) {
-            Auth = 'Unknown'
+        if (author.includes('(R.A)') || author.includes('Abu Bakr') || author.includes('Ali ibn Abi Talib') || author.includes('Umar ibn Al-Khattāb')) {
+            let arr = author.split(' ');
+            arr.unshift('Hazrat');
+            let mainAuthor = arr.join(' ');
+            // console.log(mainAuthor);
+            authorElement.innerHTML = `~ ${mainAuthor}`;
         }
-        author.innerHTML = `~&nbsp;By&nbsp;${Auth}`;
+        else authorElement.innerHTML = `~ ${author}`;
+        quoteElement.innerHTML = quote;
     }
+    catch (err) { console.log(`Error Occured: ${err} `) }
 
-    catch (err) {
-        console.log(`Error Occured: ${err} `);
-    }
-
-    let textToCopy = document.getElementById('quote').innerHTML
-
-
-    // Twitter Web Intent
-
-    let tweet = document.getElementById('twitter')
-    tweet.addEventListener('click', (e) => {
-        let twitUrl = `https://twitter.com/intent/tweet?text=${textToCopy}`
-        window.open(twitUrl, '_blank')
-    })
-
-
-    //  Copy to Clipboard Functionality
-
+    let textToCopy = document.getElementById('quote').innerHTML;
+    let tweet = document.getElementById('twitter');
     let copyBtn = document.getElementById('copyBtn')
-    let faRegular = document.getElementsByClassName('fa-regular')[0]
-    // console.log(faRegular);
-    // console.log(copyBtn);
-    copyBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        navigator.clipboard.writeText(textToCopy)
-    })
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Twitter Web Intent Functionality
+    tweet.addEventListener('click', () => window.open(`https://twitter.com/intent/tweet?text=${textToCopy}`, '_blank'))
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Copy to Clipboard Functionality
+    copyBtn.addEventListener('click', () => navigator.clipboard.writeText(textToCopy))
 }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> On Generate button Click adding Quotes Funciton
+generateButton.addEventListener('click', Quotes)
 
-button.addEventListener('click', Quotes)
 
-
-let copyBtn = document.getElementById('copyBtn')
-let faRegular = document.getElementsByClassName('fa-regular')[0]
-let tooltip = document.getElementById('tooltip')
-
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tooltip Functionality
 copyBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     faRegular.classList.toggle('fa-solid')
     tooltip.style.visibility = 'visible'
     setTimeout(() => {
         faRegular.classList.remove('fa-solid')
         tooltip.style.visibility = 'hidden'
     }, 2000)
+})
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Dark Mode Functionality
+
+// console.log(window.getComputedStyle(document.body).getPropertyValue('background-color'))
+// console.log(getStyle(body).getPropertyValue('background-color')  )
+
+darkModeBtn.addEventListener('click', () => {
+    // console.log(getStyle(body).getPropertyValue('background-color'))
+    darkModeBtn.classList.toggle("toggleBtn")
+    if (getStyle(body).getPropertyValue('background-color') == 'rgb(255, 255, 255)') {
+        // console.log('yes')
+        body.style.backgroundColor = 'rgb(64, 64, 64)';
+        box.style.backgroundColor = 'rgb(41, 41, 41)';
+        copyBtn.style.color = 'white';
+        quoteElement.style.color = 'white';
+        authorElement.style.color = 'white';
+
+    }
+    else {
+        // console.log('no')
+        body.style.backgroundColor = 'rgb(255, 255, 255)';
+        box.style.backgroundColor = 'rgb(249, 237, 237)';
+        copyBtn.style.color = 'black';
+        authorElement.style.color = 'rgb(94, 94, 94)';
+        quoteElement.style.color = 'black';
+    }
 })
